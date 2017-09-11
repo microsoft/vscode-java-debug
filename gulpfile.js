2dc3a4975faaaf99646afd0f9a142458a55673f2
Yaohai Zheng <yaozheng@microsoft.com>
2017-09-11 16:49:45 +0800

    Add binary extension. (#3)

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,28 @@
 const gulp = require("gulp");
+const cp = require('child_process');
 const tslint = require("gulp-tslint");
+const copy = require("gulp-copy");
+
+const server_dir = '../java-debug';
 
 gulp.task("tslint", () => {
-    return gulp.src(["**/*.ts", "!**/*.d.ts", "!node_modules/**", "!./src/views/node_modules/**"])
-        .pipe(tslint())
-        .pipe(tslint.report());
+	return gulp.src(["**/*.ts", "!**/*.d.ts", "!node_modules/**", "!./src/views/node_modules/**"])
+		.pipe(tslint())
+		.pipe(tslint.report());
 });
+
+gulp.task('build_server', () => {
+	cp.execSync(mvnw() + ' clean package', {
+		cwd: server_dir,
+		stdio: [0, 1, 2]
+	});
+	gulp.src(server_dir + '/com.microsoft.java.debug.plugin/target/com.microsoft.java.debug.*.jar')
+		.pipe(copy('./server', {
+			prefix: 4
+		}))
+		.pipe(gulp.dest('.'));
+});
+
+function mvnw() {
+	return "mvn";
+}
