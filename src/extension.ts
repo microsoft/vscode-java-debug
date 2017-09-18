f67b85d488f25857e9552129e89661c5843ccc58
Yaohai Zheng <yaozheng@microsoft.com>
2017-09-18 10:36:39 +0800

    Refine codebase.

--- src/extension.ts
+++ src/extension.ts
@@ -6,18 +6,18 @@ export function activate(context: vscode.ExtensionContext) {
     vscode.commands.registerCommand(commands.JAVA_START_DEBUGSESSION, async (config) => {
         if (config.request === "launch") {
             if (!config.mainClass) {
-                vscode.window.showErrorMessage("Please specify the main class in launch.json.");
+                vscode.window.showErrorMessage("Please specify the mainClass in the launch.json.");
                 return;
             } else if (!config.classPaths || !Array.isArray(config.classPaths) || !config.classPaths.length) {
                 config.classPaths = await resolveClasspath(config.mainClass, config.projectName);
             }
             if (!config.classPaths || !Array.isArray(config.classPaths) || !config.classPaths.length) {
-                vscode.window.showErrorMessage("Cannot resolve the classpaths automatically, please specify it in launch.json.");
+                vscode.window.showErrorMessage("Cannot resolve the classpaths automatically, please specify the value in the launch.json.");
                 return;
             }
         } else if (config.request === "attach") {
             if (!config.hostName || !config.port) {
-                vscode.window.showErrorMessage("Please specify the host name and the port of the remote debuggee in launch.json.");
+                vscode.window.showErrorMessage("Please specify the host name and the port of the remote debuggee in the launch.json.");
                 return;
             }
         }
@@ -25,6 +25,9 @@ export function activate(context: vscode.ExtensionContext) {
         if (debugServerPort) {
             config.debugServer = debugServerPort;
             vscode.commands.executeCommand(commands.VSCODE_STARTDEBUG, config);
+        } else {
+            // Information for diagnostic:
+            console.log("Cannot find a port for debugging session");
         }
     });
 }
