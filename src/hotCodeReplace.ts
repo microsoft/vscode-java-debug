c7772d9d9177bb2eda9b40d4bc8bfec688dba437
Yaohai Zheng <yaozheng@microsoft.com>
2017-12-15 15:21:56 +0800

    Fix const value.

--- src/hotCodeReplace.ts
+++ src/hotCodeReplace.ts
@@ -9,17 +9,23 @@ const suppressedReasons: Set<string> = new Set();
 
 const NOT_SHOW_AGAIN: string = "Not show again";
 
+const JAVA_LANGID: string = "java";
+
+const HCR_EVENT = "hotCodeReplace";
+
+const SAVEDOCUMENT_EVENT = "saveDocument";
+
 export function startHotCodeReplace(context: vscode.ExtensionContext) {
     context.subscriptions.push(vscode.debug.onDidStartDebugSession((session) => {
         const t = session ? session.type : undefined;
-        if (t === "java") {
+        if (t === JAVA_LANGID) {
             runningSessions.add(session);
         }
     }));
 
     context.subscriptions.push(vscode.debug.onDidTerminateDebugSession((session) => {
         const t = session ? session.type : undefined;
-        if (t === "java") {
+        if (t === JAVA_LANGID) {
             runningSessions.delete(session);
             suppressedReasons.clear();
         }
@@ -27,8 +33,8 @@ export function startHotCodeReplace(context: vscode.ExtensionContext) {
 
     context.subscriptions.push(vscode.debug.onDidReceiveDebugSessionCustomEvent((customEvent) => {
         const t = customEvent.session ? customEvent.session.type : undefined;
-        if (t === "java") {
-            if (customEvent.event === "hotCodeReplace" && !suppressedReasons.has(customEvent.body.message)) {
+        if (t === JAVA_LANGID) {
+            if (customEvent.event === HCR_EVENT && !suppressedReasons.has(customEvent.body.message)) {
                 vscode.window.showInformationMessage(`Hot code replace failed - ${customEvent.body.message}`, NOT_SHOW_AGAIN).then((res) => {
                     if (res === NOT_SHOW_AGAIN) {
                         suppressedReasons.add(customEvent.body.message);
@@ -39,9 +45,9 @@ export function startHotCodeReplace(context: vscode.ExtensionContext) {
     }));
 
     context.subscriptions.push(vscode.workspace.onDidSaveTextDocument((e) => {
-        if (e.languageId === "java") {
+        if (e.languageId === JAVA_LANGID) {
             runningSessions.forEach((session) => {
-                return session.customRequest("saveDocument", { documentUri: e.uri.toString() }).then(() => {
+                return session.customRequest(SAVEDOCUMENT_EVENT, { documentUri: e.uri.toString() }).then(() => {
                 }, () => { });
             });
         }
