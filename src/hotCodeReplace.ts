85b8f23d8fed9a081391aa6f13d8ce6ccc98b9d3
Yaohai Zheng <yaozheng@microsoft.com>
2017-12-18 16:32:44 +0800

    Refine HCR event.

--- src/hotCodeReplace.ts
+++ src/hotCodeReplace.ts
@@ -15,6 +15,13 @@ const HCR_EVENT = "hotCodeReplace";
 
 const SAVEDOCUMENT_EVENT = "saveDocument";
 
+enum HcrEventType {
+    ERROR = "ERROR",
+    WARNING = "WARNING",
+    STARTING = "STARTING",
+    END = "END",
+}
+
 export function startHotCodeReplace(context: vscode.ExtensionContext) {
     context.subscriptions.push(vscode.debug.onDidStartDebugSession((session) => {
         const t = session ? session.type : undefined;
@@ -34,12 +41,31 @@ export function startHotCodeReplace(context: vscode.ExtensionContext) {
     context.subscriptions.push(vscode.debug.onDidReceiveDebugSessionCustomEvent((customEvent) => {
         const t = customEvent.session ? customEvent.session.type : undefined;
         if (t === JAVA_LANGID) {
-            if (customEvent.event === HCR_EVENT && !suppressedReasons.has(customEvent.body.message)) {
-                vscode.window.showInformationMessage(`Hot code replace failed - ${customEvent.body.message}`, NOT_SHOW_AGAIN).then((res) => {
-                    if (res === NOT_SHOW_AGAIN) {
-                        suppressedReasons.add(customEvent.body.message);
+            if (customEvent.event === HCR_EVENT) {
+                if (customEvent.body.eventType === HcrEventType.ERROR || customEvent.body.eventType === HcrEventType.WARNING) {
+                    if (!suppressedReasons.has(customEvent.body.message)) {
+                        vscode.window.showInformationMessage(`Hot code replace failed - ${customEvent.body.message}`, NOT_SHOW_AGAIN).then((res) => {
+                            if (res === NOT_SHOW_AGAIN) {
+                                suppressedReasons.add(customEvent.body.message);
+                            }
+                        });
                     }
-                });
+                } else {
+                    if (customEvent.body.eventType === HcrEventType.STARTING) {
+                        vscode.window.withProgress({ location: vscode.ProgressLocation.Window }, (p) => {
+                            p.report({ message: customEvent.body.message });
+                            return new Promise((resolve, reject) => {
+                                const listener = vscode.debug.onDidReceiveDebugSessionCustomEvent((hcrEvent) => {
+                                    p.report({ message: hcrEvent.body.message });
+                                    if (hcrEvent.body.eventType === HcrEventType.END) {
+                                        listener.dispose();
+                                        resolve();
+                                    }
+                                });
+                            });
+                        });
+                    }
+                }
             }
         }
     }));
