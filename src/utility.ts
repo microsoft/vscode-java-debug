6760d1c38c7619848a0c38c41139079dad6c4bfb
Jinbo Wang <jinbwan@microsoft.com>
2018-07-27 14:08:54 +0800

    Add the missing license header

--- src/utility.ts
+++ src/utility.ts
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
 import * as opn from "opn";
 import * as vscode from "vscode";
 import { logger, Type } from "./logger";
