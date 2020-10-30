cd1e7aee780cecc0ce69d03feb7ca58fc544da1b
Sheng Chen <sheche@microsoft.com>
2020-10-30 15:06:26 +0800

    Remove unused import

--- src/utility.ts
+++ src/utility.ts
@@ -1,7 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license.
 
-import * as _ from "lodash";
 import * as path from "path";
 import * as vscode from "vscode";
 import { sendError, sendInfo, setUserError } from "vscode-extension-telemetry-wrapper";
